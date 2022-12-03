0a7de027b36539c78fddc38afc862b17ce097349
Evgenii Stratonikov <stratonikov@runbox.com>
2022-12-03 23:00:07 +0300

    day03: optimize priority calculation
    The trick here is to simplify mask calculation and postpone real priority
    calculation until the final step. It should work because mask calculation is done
    for every _character_, while we need real priority value only once per line.
    ```
    part 1                  time:   [7.2266 µs 7.3797 µs 7.5732 µs]
                            change: [-49.581% -47.313% -44.636%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    part 2                  time:   [7.8114 µs 8.0815 µs 8.3765 µs]
                            change: [-58.353% -56.702% -54.944%] (p = 0.00 < 0.05)
                            Performance has improved.
    ```
    
    Signed-off-by: Evgenii Stratonikov <stratonikov@runbox.com>

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -9,8 +9,8 @@ pub fn part1(input: &str) -> u32 {
             let p = calculate_mask(h1);
             h2.iter()
                 .find_map(|c| {
-                    let prio = priority(*c - 1);
-                    (p & (1 << prio) != 0).then_some(prio + 1)
+                    let prio = fake_priority(*c);
+                    (p & (1 << prio) != 0).then_some(fake_to_real(prio))
                 })
                 .unwrap_or(0) as u32
         })
@@ -22,22 +22,32 @@ pub fn part2(input: &str) -> u32 {
         .lines()
         .map(|s| calculate_mask(s.as_bytes()))
         .array_chunks::<3>()
-        .map(|p| (p[0] & p[1] & p[2]).trailing_zeros() + 1)
+        .map(|p| fake_to_real((p[0] & p[1] & p[2]).trailing_zeros() as u8) as u32)
         .sum()
 }
 
 /// Calculates priority mask from the items contained in b.
 fn calculate_mask(b: &[u8]) -> u64 {
-    b.iter().fold(0u64, |p, c| p | 1 << priority(c - 1))
+    b.iter().fold(0u64, |p, c| p | 1 << fake_priority(*c))
 }
 
-/// Converts item type to a priority:
+/// Converts item type to a "fake" priority:
+/// - Lowercase item types a through z have fake priorities 32 through 57.
+/// - Uppercase item types A through Z have fake priorities 1 through 26.
+/// The trick here is to simplify mask calculation and postpone real priority
+/// calculation until the final step. It should work because mask calculation is done
+/// for every _character_, while we need real priority value only once per _line_.
+fn fake_priority(a: u8) -> u8 {
+    a & 0x3F
+}
+
+/// Converts "fake" priority to a real one:
 /// - Lowercase item types a through z have priorities 1 through 26.
 /// - Uppercase item types A through Z have priorities 27 through 52.
-fn priority(a: u8) -> u8 {
-    // 'a'..'z' == 0x61..0x7A == 0b110_0001..0b111_1010
-    // 'A'..'Z' == 0x41..0x5A == 0b100_0001..0b101_1010
-    (1 - ((a >> 5) & 1)) * 26 + (a & 0x1F)
+fn fake_to_real(p: u8) -> u8 {
+    // 'a'..'z' have fake priorities in range 0b10_0001..0b11_1010
+    // 'A'..'Z' have fake priorities in range 0b00_0001..0b01_1010
+    (1 - ((p >> 5) & 1)) * 26 + (p & 0x1F)
 }
 
 #[cfg(test)]
