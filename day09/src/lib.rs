9b9eef9a912a3af92a4d3eb3d4598171ba3d7add
Evgenii Stratonikov <stratonikov@runbox.com>
2022-12-09 08:54:59 +0300

    day09: initial implementation
    Signed-off-by: Evgenii Stratonikov <stratonikov@runbox.com>

---
+++ day09/src/lib.rs
@@ -0,0 +1,165 @@
+#![feature(array_chunks)]
+pub fn part1(input: &str) -> u32 {
+    let mut seen = std::collections::HashSet::new();
+    seen.insert(0);
+
+    let mut head = (0i32, 0i32);
+    let mut tail = (0i32, 0i32);
+    // We spent most of the time inserting items in a map.
+    // When we change the direction (~20% on real input) often the tail doesn't move.
+    // So we remember the last key we have seen and do not try to insert it in the map
+    // if it hasn't changed.
+    let mut last_key = 0;
+    input.as_bytes().split(|&c| c == b'\n').for_each(|b| {
+        if b.len() < 3 {
+            return;
+        }
+
+        let d = match b[0] {
+            b'R' => (1, 0),
+            b'L' => (-1, 0),
+            b'U' => (0, 1),
+            b'D' => (0, -1),
+            _ => (0, 0),
+        };
+
+        for _ in 0..int_from_bytes::<u8>(&b[2..]) {
+            head = (head.0 + d.0, head.1 + d.1);
+
+            (head, tail) = move_points(head, tail);
+
+            let key = ((tail.0 as i16) << 8) | (tail.1 as i16 & 0xFF);
+            if key != last_key {
+                seen.insert(key);
+            }
+            last_key = key;
+        }
+    });
+
+    seen.len() as u32
+}
+
+pub fn move_points(head: (i32, i32), tail: (i32, i32)) -> ((i32, i32), (i32, i32)) {
+    let mut h = head;
+    let mut t = tail;
+
+    let h0eq2 = h.0 & 0x3 == 2;
+    let h1eq2 = h.1 & 0x3 == 2;
+    t.0 += h.0.signum() * (h0eq2 || h.0 & 0x1 == 1 && h1eq2) as i32;
+    t.1 += h.1.signum() * (h1eq2 || h.1 & 0x1 == 1 && h0eq2) as i32;
+
+    if h0eq2 {
+        h.0 /= 2;
+        h.1 *= h1eq2 as i32;
+    }
+    if h1eq2 {
+        h.1 /= 2;
+        h.0 *= h0eq2 as i32;
+    }
+    (h, t)
+}
+
+pub fn part2(input: &str) -> u32 {
+    let mut seen = std::collections::HashSet::new();
+    seen.insert(0);
+
+    let mut points = [(0, 0); 10];
+    let mut last = [(0, 0); 10];
+    input.as_bytes().split(|&c| c == b'\n').for_each(|b| {
+        if b.len() < 3 {
+            return;
+        }
+
+        let d = match b[0] {
+            b'R' => (1, 0),
+            b'L' => (-1, 0),
+            b'U' => (0, 1),
+            b'D' => (0, -1),
+            _ => (0, 0),
+        };
+        'outer: for _ in 0..int_from_bytes::<u8>(&b[2..]) {
+            points[0].0 += d.0;
+            points[0].1 += d.1;
+            for i in 1..points.len() {
+                (points[i - 1], points[i]) = move_points(points[i - 1], points[i]);
+                if last[i] == points[i] {
+                    continue 'outer;
+                }
+                last[i - 1] = points[i - 1];
+            }
+
+            let tail = points[points.len() - 1];
+            if last[last.len() - 1] == tail {
+                continue 'outer;
+            }
+            last[last.len() - 1] = tail;
+
+            let key = ((tail.0 as i16) << 8) | (tail.1 as i16 & 0xFF);
+            seen.insert(key);
+        }
+    });
+    seen.len() as u32
+}
+
+fn int_from_bytes<T>(s: &[u8]) -> T
+where
+    T: From<u8> + std::ops::Mul<T, Output = T> + std::ops::Add<T, Output = T>,
+{
+    s.iter().fold(T::from(0), |n, c| {
+        let r = match c {
+            b'0' => Some(T::from(0)),
+            b'1' => Some(T::from(1)),
+            b'2' => Some(T::from(2)),
+            b'3' => Some(T::from(3)),
+            b'4' => Some(T::from(4)),
+            b'5' => Some(T::from(5)),
+            b'6' => Some(T::from(6)),
+            b'7' => Some(T::from(7)),
+            b'8' => Some(T::from(8)),
+            b'9' => Some(T::from(9)),
+            _ => None,
+        };
+        n * T::from(10) + r.unwrap()
+    })
+}
+
+pub fn run_part1() {
+    println!("{}", part1(include_str!("../input")));
+}
+
+pub fn run_part2() {
+    println!("{}", part2(include_str!("../input")));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn example() {
+        let input = "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
+
+        assert_eq!(13, part1(&input));
+        assert_eq!(1, part2(&input));
+    }
+
+    #[test]
+    fn large_example() {
+        let input = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+        assert_eq!(36, part2(&input));
+    }
+}
