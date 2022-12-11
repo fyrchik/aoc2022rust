76bb0a3b4303bbdc77fc6f8799a9502fda12afe1
Evgenii Stratonikov <stratonikov@runbox.com>
2022-12-11 21:23:45 +0300

    day11: fix `main.rs`
    Signed-off-by: Evgenii Stratonikov <stratonikov@runbox.com>

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,9 +2,6 @@ use day11::*;
 
 fn main() {
     let input = include_str!("../input");
-    for _ in 0..10_000 {
-        part2(input);
-    }
-    //  println!("Part 1: {}", part1(input));
-    //  println!("Part 2: {}", part2(input));
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
