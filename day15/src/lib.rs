c19d0169764f9bafc51f95a549884cb9a982ac5b
Evgenii Stratonikov <stratonikov@runbox.com>
2022-12-16 07:44:31 +0300

    day15: reimplement part 2
    ```
    part 2                  time:   [2.9215 µs 2.9963 µs 3.0880 µs]
                            change: [-99.999% -99.998% -99.998%] (p = 0.00 < 0.05)
                            Performance has improved.
    ```
    
    Signed-off-by: Evgenii Stratonikov <stratonikov@runbox.com>

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -35,10 +35,12 @@ pub fn part1(input: &str, row: isize) -> u32 {
             Some(sx - d..sx + d + 1)
         })
         .collect();
-    ranges.sort_by(|a, b| a.start.cmp(&b.start).then(b.end.cmp(&a.end)));
-    if ranges.len() == 0 {
+
+    if ranges.is_empty() {
         return 0;
     }
+
+    ranges.sort_by(|a, b| a.start.cmp(&b.start).then(b.end.cmp(&a.end)));
 
     let mut count = 0;
     let mut last = ranges[0].start;
@@ -61,7 +63,6 @@ pub fn part1(input: &str, row: isize) -> u32 {
 }
 
 pub fn part2(input: &str, max: isize) -> u64 {
-    let mut beacons = vec![];
     let mut sensors = vec![];
     input.lines().for_each(|s| {
         // Example: Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -78,33 +79,62 @@ pub fn part2(input: &str, max: isize) -> u64 {
         start += bx_len + 4; // ", y="
 
         let (by, _) = int_from_bytes_prefix::<isize>(&b[start..]);
-        let index = beacons.partition_point(|&x| x < bx);
-        if index == beacons.len() || beacons[index] != bx {
-            beacons.insert(index, bx);
-        }
 
         let dist = (sx.abs_diff(bx) + sy.abs_diff(by)) as isize;
         if max < sy - dist || sy + dist < 0 {
             return;
         }
-        sensors.push((sx, sy, dist));
+        sensors.push(((sx, sy), dist));
     });
 
-    for y in 0..=max {
-        let mut x = 0;
-        'xloop: while x <= max {
-            for s in &sensors {
-                let d = s.2 - s.1.abs_diff(y) as isize;
-                if s.0 - d <= x && x <= s.0 + d {
-                    x = s.0 + d + 1;
-                    continue 'xloop;
-                }
+    let mut edges = vec![];
+    for (i, a) in sensors.iter().enumerate() {
+        for b in sensors.iter().skip(i + 1) {
+            // If there is a _single_ point, it must be located between two boundaries.
+            let dist = (a.0 .0.abs_diff(b.0 .0) + a.0 .1.abs_diff(b.0 .1)) as isize;
+            if dist == a.1 + b.1 + 2 {
+                edges.push(if a.0 < b.0 { (*a, *b) } else { (*b, *a) });
             }
-            return x as u64 * 4_000_000 + y as u64;
+        }
+    }
+    for (i, &(s1, s2)) in edges.iter().enumerate() {
+        for &(s3, s4) in edges.iter().skip(i + 1) {
+            // if p0.2.0 == p1.2.0 || p0.2.0 == p1.2.1 || p0.2.1 == p1.2.0 || p0.2.1 == p1.2.1 {
+            //    continue
+            // }
+            if intersect2d(s1.0, s2.0, s3.0, s4.0) {
+                let (x1, y1) = (s1.0 .0 + s1.1 + 1, s1.0 .1);
+                let (x2, y2) = (s2.0 .0 - s1.1 - 1, s2.0 .1);
+                let dist = (x1.abs_diff(s3.0 .0) + y1.abs_diff(s3.0 .1)) as isize;
+                let dist_diff = dist.abs_diff(s3.1 + 1) as isize / 2;
+                let x = x1 + (x2 - x1).signum() * dist_diff;
+                let y = y1 + (y2 - y1).signum() * dist_diff;
+                return x as u64 * 4_000_000 + y as u64;
+            }
         }
     }
 
     u64::MAX
+}
+
+fn intersect2d(a: (isize, isize), b: (isize, isize), c: (isize, isize), d: (isize, isize)) -> bool {
+    if !intersect1d(a.0, b.0, c.0, d.0) || !intersect1d(a.1, b.1, c.1, d.1) {
+        return false;
+    }
+
+    let v_ab = (b.0 - a.0, b.1 - a.1);
+    let v_ac = (c.0 - a.0, c.1 - a.1);
+    let v_ad = (d.0 - a.0, d.1 - a.1);
+
+    let det_c = v_ab.0 * v_ac.1 - v_ab.1 * v_ac.0;
+    let det_d = v_ab.0 * v_ad.1 - v_ab.1 * v_ad.0;
+    det_c.signum() * det_d.signum() == -1
+}
+
+fn intersect1d(a: isize, b: isize, c: isize, d: isize) -> bool {
+    let (a, b) = (a.min(b), a.max(b));
+    let (c, d) = (c.min(d), c.max(d));
+    a.max(c) <= b.min(d)
 }
 
 pub fn run_part1() {
